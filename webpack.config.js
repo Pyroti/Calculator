e0fe07aec3d26ee576f556dba9a854309466fc0c
Pyroti <pavel.gangin@gmail.com>
2021-07-19 17:28:22 +0300

    feat: remove index

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: './assets/html/first.html'
     }),
     new CleanWebpackPlugin()
   ],
